f45102021af7be32894d7a70aff099f2bb14f4a8
piotrkowandy <piotr.kowandy@o2.pl>
2019-07-12 12:52:37 +0200

    webkit updated

--- codeStyles/WebKitStyle.cpp
+++ codeStyles/WebKitStyle.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-int main() {
-  int a, b, c;
-  cout << "Enter value of a: ";
-  cin >> a;
-  cout << "Enter value of b: ";
-  cin >> b;
-  c = a;
-  a = b;
-  b = c;
-  cout << "After swaping a: " << a << " b: " << b;
-  return 0;
+int main()
+{
+    int a, b, c;
+    cout << "Enter value of a: ";
+    cin >> a;
+    cout << "Enter value of b: ";
+    cin >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << "After swaping a: " << a << " b: " << b;
+    return 0;
 }
 
 /*
